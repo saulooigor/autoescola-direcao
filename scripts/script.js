caf31b9fe931fc9e670dbb0c294f3422d4c45473
Saulo Igor Silva Alves Do Carmo <saulo.carmo@mrv.com.br>
2024-04-08 16:47:02 -0300

    Refactor presentation of package values and fix styling issues

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
  $(document).ready(function(){
-    $('.card').click(function(){
-        $('.card').removeClass('border-black');
-        $('.card').find('.card-img-top').css('opacity', '.3');
-        $('.card').find('.card-body').css('background-color', 'white');
-        $('.card').find('.card-body').css('color', 'black');
+    $('#categorias .card').click(function(){
+        $('#categorias .card').removeClass('border-black');
+        $('#categorias .card').find('.card-img-top').css('opacity', '.3');
+        $('#categorias .card').find('.card-body').css('background-color', 'white');
+        $('#categorias .card').find('.card-body').css('color', 'black');
 
         $(this).addClass('border-black');
         $(this).find('.card-img-top').css('opacity', '1');
